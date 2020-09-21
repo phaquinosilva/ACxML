0ffda4e4d387cb85504abf535a0971f8d61a97e9
Pedro Aquino <phaquino1998@gmail.com>
2020-09-21 18:16:03 -0300

    agora com mais pandas

--- logical simulation/8bit.py
+++ logical simulation/8bit.py
@@ -1,36 +1,48 @@
 # from bistring import BitArray
-import adders
+import adders as add
+import arc_gen as primes
+import pandas as pd
 
-# vou começar escrevendo um RCA 4bit
-def approx_sum(op_a, op_b, adder_type, n_bits):
-    # passo operandos para binário para operar bit a bit
-    # formatação para decidirmos número bits
+
+# RCA nbit
+def approx_sum(op_a, op_b, n_bits):
+
+    # bem, não pensei em outro jeito melhor de iterar sobre essas funções, então cá estamos
+    adder = [add.sma, add.ama1, add.ama2, add.axa2, add.axa3]
+
+    # passo operandos para binário de n_bits
     form = '#0' + str(n_bits + 2) + 'b'
-    op_a = format(op_a, form)[2:]
-    op_b = format(op_b, form)[2:]
+    in_a = format(op_a, form)[2:]
+    in_b = format(op_b, form)[2:]
+    results = {
+        "a" : op_a,
+        "b" : op_b,
+        "exact" : op_a + op_b
+    }
+    for a in adder:
+        cin = 0
+        final = ''
+        for i in range(len(in_a)-1, -1, -1):
+            fa = a(int(in_a[i]), int(in_b[i]), cin)
+            final += str(fa[0])
+            cin = fa[1]
+        # já que se adiciona iterativamente do LSB até o MSB, invertemos a bistring do resultado
+        # converte para binário, para permitir comparação mais fácil com o exato
+        final = int(final[::-1], 2)             # esse método não considera complemento de dois
+        # b = BitArray(bin=final[::-1])
+        # final = b.int                    # já esse b.int considera
+        # adiciona resultados a um dict para criar o DF
+        results[a.__name__] = final
+    return results
 
-    cin = 0
-    result = ''
+# executa cada soma e organiza resultados em um df que sera escrito em
+def sum_primes(approx, num, bit_len):
+    sums = primes.generate_sums(approx, num)
+    s_list = []
+    for s in sums:
+        s_list.append(approx_sum(s[0], s[1], bit_len))
+    return pd.DataFrame(s_list)
 
-    for i in range(len(op_a)-1, -1, -1):
-        fa = adder_type(int(op_a[i]), int(op_b[i]), cin)
-        print("In: " + str((int(op_a[i]), int(op_b[i]), cin)))
-        print("Out: " + str(fa))
-        result += str(fa[0])
-        cin = fa[1]
-
-    # já que se adiciona iterativamente do LSB até o MSB, invertemos a bistring do resultado
-    result = result[::-1]
-
-    # converte para binário, para permitir comparação mais fácil com o exato
-    result = int(result, 2)             # esse método não considera complemento de dois
-    # b = BitArray(bin=result)
-    # result = b.int                    # já esse int considerou C2
-    return result
-
-# teste:
-# print(approx_sum(14, 6, adders.axa3, 6))
-
-## algumas ideias:
-# → posso inverter os dois operandos e fazer o 'for' crescente, mas ainda precisaria reverter o result
-# → gostaria de conseguir iterar sobre as funções de cada topologia, pra extrair todos os resultados de uma vez para cada soma
+# testes:
+# print(approx_sum(14, 6, 6))
+# print(sum_primes(True, 20, 6))
