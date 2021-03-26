d2e6d4b4b1ca6dbc37ddfc67baaeed2312a7baa5
Pedro <phaquino1998@gmail.com>
2021-03-25 21:37:28 -0300

    dedicated simulation ready

--- comparators/hspice/pyspice.py
+++ comparators/hspice/pyspice.py
@@ -7,13 +7,10 @@ from random import sample
 ################################# HSPICE SCRIPT #######################################
 
 # executa simulacoes
-def run_hspice(comparator, comp_num, cell=None):
+def run_adders(comparator, comp_num, cell):
     # sets type of sources file and name of output file
     source = '.include sources/'
-    if cell == None:
-        name = 'results/result_' + comparator + '_' + str(comp_num) +'.csv'
-        source +='source_exact_' + str(comp_num) + '.cir'
-    elif cell == 'exa' or cell == 'ema':
+    if cell == 'exa' or cell == 'ema':
         name = 'results/result_' + comparator + '_' + cell + '_' + str(comp_num) +'.csv'
         source +='source_exact_' + str(comp_num) + '.cir'
         adder = '.include fas/' + cell + '.cir\n'
@@ -27,42 +24,12 @@ def run_hspice(comparator, comp_num, cell=None):
     with open('./nondef_params.txt', 'w') as f:
         f.seek(0)
         f.write(source)
-        if cell != None: f.write(adder)
+        f.write(adder)
     # chamadas de sistema pra executar o hspice
     os.system('hspice '+ comparator + '.cir')
     os.rename('./' + comparator + '.mt0.csv',  name)
 
-# organiza dados de um output .csv do HSPICE
-def organize_results(sim_time, voltage, comparator=None, cell=None):
-    if cell == None or comparator == None: 
-        return organize_dedicated(sim_time, voltage)
-    else:
-        return organize_adders(sim_time, voltage, comparator, cell)
-
 def organize_adders(sim_time, voltage, comparator, cell):
-    adder_results = []
-    p = Path('.')
-    for csv in list(p.glob('**/result_' + comparator+'_' + cell+'_*.csv')):
-        res_df = pd.read_csv(csv, skiprows=3, na_values="failed")
-        print(res_df)
-        # seleciona colunas relevantes
-        delay_df = res_df.filter(regex='tp')
-        if cell == "AXA2" or cell == "AXA3" or cell == "EXA":
-            power = (-1) * (res_df['q_dut'].iloc[0] + res_df['q_in'].iloc[0]) * voltage / sim_time
-        else:
-            power = (-1) * res_df['q_dut'].iloc[0] * voltage / sim_time
-        # pior caso de atraso
-        delay = delay_df.max(axis=1).iloc[0]
-        adder_results.append({'delay' : delay, 'power' : power})
-        # limpa diretorio para restarem somente os arquivos relevantes
-        os.remove(csv)
-    sums_res = pd.DataFrame(adder_results)
-    avg_pow = sums_res['power'].mean()
-    delay = sums_res['delay'].max(axis=0)
-    return {'delay' : delay, 'power' : avg_pow}
-
-
-def organize_dedicated(sim_time, voltage, comparator, cell):
     adder_results = []
     p = Path('.')
     for csv in list(p.glob('**/result_' + comparator+'_' + cell+'_*.csv')):
@@ -100,7 +67,7 @@ def adders_sim():
             run_hspice('comp_subtractor', j, fa[i])
         # retorna arquivo pro original
         post(fa[i])
-        results[fa[i]] = organize_results(5e-9, 0.7, 'comp_subtractor', fa[i])
+        results[fa[i]] = organize_adders(5e-9, 0.7, 'comp_subtractor', fa[i])
     prime = pd.DataFrame(results)
     prime.to_csv('./results/comp_subtractor_results.csv')
 
@@ -128,11 +95,48 @@ def dedicated_sim():
     for i in range(len(sample_sizes)) :
         # simulação para subtratores
         for j in range(sample_sizes):
-            run_hspice(comparator=comparators[i], comp_num=j)
-        results = organize_results(5e-9, 0.7, comparators[i])
+            run_dedicated(comparator=comparators[i], comp_num=j)
+        results[comparators[i]] = organize_dedicated(5e-9, 0.7, comparators[i])
     prime = pd.DataFrame(results)
     prime.to_csv('./results/' + comparator + '_results.csv')
 
+def run_dedicated(comparator, comp_num):
+    name = 'results/result_' + comparator + '_' + str(comp_num) +'.csv'
+    source = '.include source'+comparator+'_'+str(comp_num)+'.cir\n'
+    if comparator = 'comp_exact':
+        with open('nondef_params.txt', 'w') as f:
+            f.seek(0)
+            f.write(source)
+        os.system('hspice comp_dedicated.cir')
+        os.rename('./comp_dedicated.mt0.csv',  name)
+    else: 
+        with open('nondef_params.txt', 'w') as f:
+            f.seek(0)
+            f.write('.include comparators/4bit_'+comparator+'.cir\n')
+            f.write(source)
+        os.system('hspice comp_dedicated_approx.cir')
+        os.rename('./comp_dedicated_approx.mt0.csv',  name)
+
+
+def organize_dedicated(sim_time, voltage, comparator):
+    comp_results = []
+    p = Path('.')
+    for csv in list(p.glob('**/result_'+comparator+'_*.csv')):
+        res_df = pd.read_csv(csv, skiprows=3, na_values="failed")
+        print(res_df)
+        # seleciona colunas relevantes
+        delay_df = res_df.filter(regex='tp')
+        power = (-1) * res_df['q_dut'].iloc[0] * voltage / sim_time
+        # pior caso de atraso
+        delay = delay_df.max(axis=1).iloc[0]
+        comp_results.append({'delay' : delay, 'power' : power})
+        # limpa diretorio para restarem somente os arquivos relevantes
+        os.remove(csv)
+    sums_res = pd.DataFrame(comp_results)
+    avg_pow = sums_res['power'].mean()
+    delay = sums_res['delay'].max(axis=0)
+    return {'delay' : delay, 'power' : avg_pow}
+
 if __name__ == '__main__':
-    adders_sim()
-    # dedicated_sim()
+    # adders_sim()
+    dedicated_sim()
